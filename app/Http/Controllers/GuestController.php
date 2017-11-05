7d15d25ea6a0d7a8f1c9d0b5f27fd4eaa22055e1
Andy Bigler <andy.bigler@besonet.ch>
2017-11-05 12:05:30 +0100

    Added CRUD Controller and Faq

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GuestController extends Controller
+{
+    public function postIndex()
+    {
+
+    }
+
+    public function postShow()
+    {
+
+    }
+
+    public function faqIndex()
+    {
+
+    }
+}
